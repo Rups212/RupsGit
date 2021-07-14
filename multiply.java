1a8a2bebd1adf2a4c0cfc4f8c6d71905c683a8fb
Rupesh Bangar <bangarrupesh212@gmail.com>
2021-07-14 10:45:44 +0530

    Initial file Multiply.java

---
+++ multiply.java
@@ -0,0 +1,10 @@
+public class Multiply{
+
+    public static void main(String[] args)
+    {
+        
+         int a=5;
+        int b=10;
+        system.out.println("The multiplication of is :"+(a*b));
+    }
+}
